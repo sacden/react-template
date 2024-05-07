f63672697c3c56bba41132373ad873b9dca61cf7
sacden <sacden2009@gmail.com>
2024-05-07 14:04:31 +0200

    Step 12 - adding css & images

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles.css";
+import IMAGE from "./log.png";
+import LOGO from "./logo.svg";
+
 export const App = () => {
-  return <h1>React Template</h1>;
+  return (
+    <>
+      <h1>React Template</h1>
+      <img src={IMAGE} alt="logo" width="100" height="100" />
+      <img src={LOGO} alt="logo" width="100" height="100" />
+    </>
+  );
 };
